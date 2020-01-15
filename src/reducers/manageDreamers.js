48d9e3c63e14a710475b014b58e229b503bb72e2
Michael Schiumo <mschiumo18@gmail.com>
2020-01-15 17:46:46 -0500

    coded out reducer for adding and deleting aspirations

---
+++ src/reducers/manageDreamers.js
@@ -0,0 +1,33 @@
+import uuid from 'uuid';
+// export const cuidFn = cuid;
+
+export default function manageDreamers(state = {
+  aspirations: [],
+}, action) {
+
+
+  switch (action.type) {
+    case 'ADD_ASPIRATION':
+
+    const aspiration = {
+      title: action.title,
+      category: action.category,
+      timeframe: action.timeframe,
+      status: action.status,
+      id: uuid()
+    };
+
+    return {
+      ...state, aspirations: [...state.aspirations, aspiration]
+    }
+
+    case 'DELETE_ASPIRATION':
+
+    const aspirations = state.aspirations.filter(aspiration => aspiration.id !== action.id);
+
+    return {...state, aspirations}
+
+    default:
+      return state
+  }
+}
