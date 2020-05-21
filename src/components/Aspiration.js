8d84c45351d1778bef656835aae74df447c5bd9a
Michael Schiumo <mschiumo18@gmail.com>
2020-05-21 13:27:11 -0400

    changed div styles

--- src/components/Aspiration.js
+++ src/components/Aspiration.js
@@ -1,44 +1,57 @@
 import React, { Component } from 'react';
 
-
 class Aspiration extends Component {
+  handleOnClick = (event) => {
+    console.log('delete aspiration handle submit');
+    this.props.deleteAspirations(this.props.aspiration);
+  };
 
-  handleOnClick = event => {
-    console.log("delete aspiration handle submit")
-  this.props.deleteAspirations(this.props.aspiration);
-}
+  // this.props.deleteAspirations(this.props.aspiration, this.props.history);
 
-// this.props.deleteAspirations(this.props.aspiration, this.props.history);
-
-render() {
-  const { aspiration, deleteAspirations } = this.props;
-  console.log('aspiration render')
-  return (
-    <div>
-
-        <div className="card" key={aspiration.id}>
-          <br /><h2>{aspiration.title}</h2>
-          <b><label>Category</label></b>
-          <i><p>{aspiration.category}</p></i>
-          <b><label>Timeframe</label></b>
-          <i><p>{aspiration.timeframe} days</p></i>
-          <b><label>Status</label></b>
-          <i><p>{aspiration.status}</p></i><br />
+  render() {
+    const { aspiration, deleteAspirations } = this.props;
+    console.log('aspiration render');
+    return (
+      <div style={aspirationStyle}>
+        <div className='card' key={aspiration.id}>
+          <br />
+          <h2>{aspiration.title}</h2>
+          <b>
+            <label>Category</label>
+          </b>
+          <i>
+            <p>{aspiration.category}</p>
+          </i>
+          <b>
+            <label>Timeframe</label>
+          </b>
+          <i>
+            <p>{aspiration.timeframe} days</p>
+          </i>
+          <b>
+            <label>Status</label>
+          </b>
+          <i>
+            <p>{aspiration.status}</p>
+          </i>
+          <br />
           <button onClick={this.handleOnClick}></button>
         </div>
-
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-}
-
+const aspirationStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1 rem',
+};
 
 export default Aspiration;
 
 // rerender using history
 //pass down history
 //history comes from component with routes
-
 
 //create navbar
--- src/components/Aspirations.js
+++ src/components/Aspirations.js
@@ -1,54 +1,51 @@
 import React, { Component } from 'react';
 import Aspiration from './Aspiration';
 import { connect } from 'react-redux';
-import { fetchAspirations, deleteAspirations } from '../actions/aspirationsActions';
-
+import {
+  fetchAspirations,
+  deleteAspirations,
+} from '../actions/aspirationsActions';
 
 class Aspirations extends Component {
-
   componentDidMount() {
-    this.props.fetchAspirations()
+    this.props.fetchAspirations();
   }
-
-
 
   render() {
     return (
-
       <div>
-        {(this.props.aspirations.length !== 1) ?
-           <h3>You have {this.props.aspirations.length} aspirations!</h3>
-                                  :
-           <h3>You have {this.props.aspirations.length} aspiration!</h3>
+        {this.props.aspirations.length !== 1 ? (
+          <h3>You have {this.props.aspirations.length} aspirations!</h3>
+        ) : (
+          <h3>You have {this.props.aspirations.length} aspiration!</h3>
+        )}
 
-        }
-
-        {(this.props.aspirations.length !== 0) ?
+        {this.props.aspirations.length !== 0 ? (
           <h5>Click the white box below to remove an aspiration.</h5>
-                                  :
-                                  <div></div>
-        }
+        ) : (
+          <div></div>
+        )}
 
         {this.props.aspirations.map((aspiration) => {
           return (
-
             <Aspiration
-            key={aspiration.id}
-            aspiration={aspiration}
-            deleteAspirations={this.props.deleteAspirations}
-        />
-
-          )
+              key={aspiration.id}
+              aspiration={aspiration}
+              deleteAspirations={this.props.deleteAspirations}
+            />
+          );
         })}
       </div>
-
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return { aspirations: state.aspirations }
-}
+const mapStateToProps = (state) => {
+  return { aspirations: state.aspirations };
+};
 
-export default connect(mapStateToProps, { fetchAspirations, deleteAspirations })(Aspirations);
+export default connect(mapStateToProps, {
+  fetchAspirations,
+  deleteAspirations,
+})(Aspirations);
 // export default connect(null, { fetchAspirations, deleteAspirations })(Aspirations);
